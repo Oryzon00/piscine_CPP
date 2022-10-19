241adc9dfa120a3e59e5d3832212ff094b206147
Adrian Jung <adrian.mael.jung@gmail.com>
2022-10-19 03:43:32 +0200

    work from home

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -6,12 +6,14 @@
 /*   By: oryzon <oryzon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 19:05:27 by oryzon            #+#    #+#             */
-/*   Updated: 2022/10/18 19:45:41 by oryzon           ###   ########.fr       */
+/*   Updated: 2022/10/19 03:34:45 by oryzon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
+
 
 
 Fixed::Fixed(void) : _value(0)
@@ -26,6 +28,22 @@ Fixed::Fixed(Fixed const & src)
 	std::cout << "Copy constructor called" << std::endl;
 	
 	*this = src;
+	return ;
+}
+
+Fixed::Fixed(const int nb)
+{
+	std::cout << "Int constructor called" << std::endl;
+
+	this->_value = nb << _fractional_bits;
+	return ;
+}
+
+Fixed::Fixed(const float nb)
+{
+	std::cout << "Float constructor called" << std::endl;
+
+	this->_value = roundf(nb * (1 << _fractional_bits));
 	return ;
 }
 
@@ -46,7 +64,7 @@ Fixed &	Fixed::operator=(const Fixed & rhs)
 
 int	Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	//std::cout << "getRawBits member function called" << std::endl;
 	
 	return (this->_value);
 }
@@ -55,5 +73,21 @@ void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	
-	(void) raw;
+	this->_value = raw;
+}
+
+int		Fixed::toInt(void) const
+{
+	return (int) (_value >> _fractional_bits);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return  ((float) _value / (1 << _fractional_bits));
+}
+
+std::ostream	&operator<<(std::ostream &out, Fixed const &param)
+{
+	out << param.toFloat();
+	return (out);
 }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,31 +6,38 @@
 /*   By: oryzon <oryzon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 19:05:29 by oryzon            #+#    #+#             */
-/*   Updated: 2022/10/18 19:59:53 by oryzon           ###   ########.fr       */
+/*   Updated: 2022/10/19 03:12:50 by oryzon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <ostream>
+
 class Fixed
 {
 	
 	public:
-		Fixed(void);
-		Fixed(Fixed const & src);
-		~Fixed(void);
+				Fixed(void);
+				Fixed(Fixed const & src);
+				Fixed(const int nb);
+				Fixed(const float nb);
+				~Fixed(void);
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		
 		Fixed	&operator=(const Fixed &rhs);
-		
-		
+
+		int		toInt(void) const;
+		float	toFloat(void) const;
 
 	private:
 		int					_value;
-		static const int	_bit = 8;
+		static const int	_fractional_bits = 8;
 };
 
+std::ostream	&operator<<(std::ostream & out, Fixed const &param);
 
 #endif
