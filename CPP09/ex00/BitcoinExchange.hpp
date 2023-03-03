4e2d55be59258ffea94c7e1b328a08bd662ad250
Adrian <adrian.mael.jung@gmail.com>
2023-03-03 23:22:30 +0100

    better error handling // custom exception with parameter

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -12,7 +12,7 @@ class FileNotOpenException : public std::exception
 	public:
 		virtual const char*	what(void) const throw()
 		{
-			return ("Error: could not open file");
+			return ("Error: could not open file.");
 		}
 };
 
@@ -30,6 +30,54 @@ class FormatErrorException : public std::exception
 	public:
 		virtual const char*	what(void) const throw()
 		{
-			return ("Error: format error in file");
+			return ("Error: format error in file => 'date | value'");
+		}
+};
+
+class BadInputDateException : public std::exception
+{
+	public:
+		virtual const char*	what(void) const throw()
+		{
+			return ("Error: bad input date");
+		}
+};
+
+
+
+class NotAPositiveNumberException : public std::exception
+{
+	public:
+		virtual const char*	what(void) const throw()
+		{
+			return ("Error: not a positive number.");
+		}
+};
+
+class ErrorTooLargeNumberException : public std::exception
+{
+	public:
+		virtual const char*	what(void) const throw()
+		{
+			return ("Error: too large a number.");
+		}
+};
+
+class BadInputValueException : public std::exception
+{
+	std::string	_msg;
+
+	public:
+		BadInputValueException(const std::string date)
+			: _msg(std::string("Error: bad input => ") + date)	{}
+		// BadInputValueException(void)
+		// {
+		// 	_msg = "test";
+		// }
+		~BadInputValueException() throw()	{}
+
+		virtual const char*	what(void) const throw()
+		{
+			return _msg.c_str();
 		}
 };
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -11,36 +11,39 @@ void	checkNbArgs(int ac)
 		throw WrongNumberOfArgumentsException();
 }
 
-void	parseValueBC(map_t& valueBC, char** av)
+void	parseDatabase(map_t& valueBC, char** av)
 {
 	(void) valueBC;
 	(void) av;
 }
 
-void	printOutput(std::string line, map_t& dic)
+void	printOutput(std::string date, float value, map_t& dic)
 {
-	std::cout << line << std::endl;
+	std::cout << "date: " << date << " | nb BC: " << value << std::endl;
+	(void) dic;
+}
 
-	std::string date;
-	char*		value_str;
-	float		value;
+void	tokenizeLine(std::string line, map_t& dic)
+{
+	std::cout << "------ " <<  line << " ------" << std::endl;
+
+	char*	date;
+	char*	value_str;
+	float	value;
 
 	date = strtok(const_cast<char*>(line.c_str()), " ");
+	if (!date)
+		throw BadInputDateException();
 	strtok(NULL, " ");
 	value_str = strtok(NULL, " ");
 	if (!value_str)
-		std::cerr << "Error: bad input => " << date << std::endl << std::endl;
-	else
-	{
-		value = std::atof(value_str);
-		if (value < 0)
-			std::cerr << "Error: not a positive number => " << date << std::endl << std::endl;
-		else if (value > 1000)
-			std::cerr << "Error: number is above 1000 => " << date << std::endl << std::endl;
-		else 
-			std::cout << "date: " << date << " | value: " << value << std::endl << std::endl;
-	} 
-	(void) dic;
+		throw BadInputValueException(date);
+	value = std::atof(value_str);
+	if (value < 0)
+		throw NotAPositiveNumberException();
+	if (value > 1000)
+		throw ErrorTooLargeNumberException();
+	printOutput(std::string(date), value, dic);
 }
 
 void	parseInputFile(map_t& dic, char** av)
@@ -55,12 +58,18 @@ void	parseInputFile(map_t& dic, char** av)
 		throw FormatErrorException();
 
 	while (getline(inputFile, line))
-		printOutput(line, dic);
+	{
+		try
+		{
+			tokenizeLine(line, dic);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
 	inputFile.close();
 }
-//seulement remplir une map avec le fichier .csv
-//puis parser ligne par ligne input.txt
-//chaque ligne: find key(date), find nbBitcoin, ecrire erreur s'il y en a
 
 int	main(int ac, char** av)
 {
@@ -68,6 +77,7 @@ int	main(int ac, char** av)
 	try
 	{
 		checkNbArgs(ac);
+		parseDatabase(dic, av);
 		parseInputFile(dic, av);
 	}
 	catch(const std::exception& e)
@@ -75,6 +85,5 @@ int	main(int ac, char** av)
 		std::cerr << e.what() << '\n';
 		return 1;
 	}
-	(void) av;
 	return 0;
 }
